84cedca26856dda4e6b1c0fa45f1aca5e6a42bc6
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2024-04-30 00:48:06 +0200

    add m2 blueprint

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,6 +1,7 @@
 import 'vuetify/styles'
 import {createVuetify} from "vuetify";
 import {aliases, mdi} from "vuetify/iconsets/mdi";
+import {md2} from "vuetify/blueprints";
 
 export default createVuetify({
   icons: {
@@ -10,6 +11,7 @@ export default createVuetify({
       mdi,
     }
   },
+  blueprint: md2,
   theme: {
     defaultTheme: 'dark',
   }

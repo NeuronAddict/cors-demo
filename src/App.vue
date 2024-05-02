dbb1972d1e0f55e4180534bb735e65cb459a476f
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2024-05-02 21:56:04 +0200

    Fix theme

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,25 @@
 <template>
-  <v-app>
+  <v-theme-provider theme="custom">
+    <v-app>
+      <v-layout class="rounded rounded-md">
+        <v-app-bar title="Application bar"></v-app-bar>
 
-    <v-layout class="rounded rounded-md">
-      <v-app-bar title="Application bar"></v-app-bar>
+        <v-navigation-drawer>
+          <v-list>
+            <v-list-item title="Navigation drawer"></v-list-item>
+          </v-list>
+        </v-navigation-drawer>
 
-      <v-navigation-drawer>
-        <v-list>
-          <v-list-item title="Navigation drawer"></v-list-item>
-        </v-list>
-      </v-navigation-drawer>
+        <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+          <MessageList/>
+        </v-main>
+      </v-layout>
 
-      <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
-        <MessageList />
-      </v-main>
-    </v-layout>
-
-
-  </v-app>
+    </v-app>
+  </v-theme-provider>
 </template>
 
 <script setup lang="ts">
-  //
-  import MessageList from "@/components/MessageList.vue";
+//
+import MessageList from "@/components/MessageList.vue";
 </script>

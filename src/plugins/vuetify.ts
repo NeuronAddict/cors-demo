f9b22af2a102f44cc36ad4e5544ef459b1950c87
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2024-05-02 22:03:20 +0200

    custom theme

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -2,6 +2,7 @@ import 'vuetify/styles'
 import {createVuetify} from "vuetify";
 import {aliases, mdi} from "vuetify/iconsets/mdi";
 import {md2} from "vuetify/blueprints";
+import colors from 'vuetify/util/colors'
 
 export default createVuetify({
   icons: {
@@ -12,8 +13,21 @@ export default createVuetify({
     }
   },
   blueprint: md2,
+  defaults: {
+    global: {
+      elevation: 4
+    }
+  },
   theme: {
-    defaultTheme: 'dark',
+    themes: {
+      custom: {
+        dark: true,
+        colors: {
+          primary: colors.purple.darken4, // #E53935
+          secondary: colors.purple.darken1, // #FFCDD2
+        }
+      },
+    },
   }
 })
 

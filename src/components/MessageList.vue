d9394614de2925cc6b0ed11fe75b6f896400e4de
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2024-05-05 03:19:45 +0200

    Review message List

--- src/components/MessageList.vue
+++ src/components/MessageList.vue
@@ -2,9 +2,9 @@
 
 import MessageBox from "@/components/MessageBox.vue";
 import type {Message} from "@/core/message";
-import {onMounted, ref} from "vue";
+import {computed, onMounted, ref} from "vue";
 
-const messages = ref([] as Message[])
+const messages = ref([] as Array<Message>)
 
 let loaded = ref(false);
 let errorReason = ref("")
@@ -12,7 +12,7 @@ let errorReason = ref("")
 onMounted(() => {
   fetch('/api/v1/messages')
       .then(value => value.json())
-      .then(value => messages.value = value)
+      .then(value => messages.value = (value as Message[]))
       .catch(reason => {
         console.log(reason);
         errorReason.value = reason;
@@ -23,31 +23,48 @@ onMounted(() => {
   ;
 });
 
+let reverseMessages = computed(() => [...messages.value].reverse());
+
+const messageInput = ref("");
+
+function sendMessage(event: Event) {
+  messages.value.push({author: "Anonyme", message: messageInput.value});
+  messageInput.value = "";
+}
+
 </script>
 
 <template>
-  <div :hidden="loaded">
-    <v-progress-circular color="primary" size="50" width="7" indeterminate></v-progress-circular>
-  </div>
+    <div :hidden="loaded">
+      <v-progress-circular color="primary" size="50" width="7" indeterminate></v-progress-circular>
+    </div>
 
-  <v-virtual-scroll :items="messages" class="messages-list">
-    <template v-slot:default="{ item }">
-      <MessageBox :author="item.author" :message="item.message">
-      </MessageBox>
-    </template>
-  </v-virtual-scroll>
+  <v-sheet class="ma-3">
+    <v-row justify="space-around" align="center" class="ma-2">
+      <v-col md="10">
+      <v-textarea rows="2" prepend-icon="mdi-task" hint="Type a task to do" auto-grow v-model="messageInput" class="ma-2" ></v-textarea>
+      </v-col>
+      <v-col md="2">
+        <v-btn @click="sendMessage" class="ma-2"><v-icon icon="mdi-plus-circle-outline"></v-icon></v-btn>
+      </v-col>
+    </v-row>
+  </v-sheet>
 
-  <div class="text-red-darken-4" :hidden="errorReason.length == 0">Error :/ {{ errorReason }}</div>
+    <v-virtual-scroll :items="reverseMessages" class="messages-list">
+      <template v-slot:default="{ item }">
+        <MessageBox :author="item.author" :message="item.message">
+        </MessageBox>
+      </template>
+    </v-virtual-scroll>
+
+
+
+    <div class="text-red-darken-4" :hidden="errorReason.length == 0">Error :/ {{ errorReason }}</div>
 </template>
 
 <style scoped>
 
 .messages-list {
-  height: 80vh;
-}
-
-.message-add {
-  height: 20vh;
 }
 
 </style>

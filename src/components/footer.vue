0afdb7258a9dd2eef02cb4b7fd16a2d9b9ce477c
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2024-05-06 00:35:50 +0200

    Add footer

---
+++ src/components/footer.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <v-footer class="d-flex">
+    <v-row justify="space-around" class="fill-width-custom">
+      <v-col cols="4" md="4" sm="12"  class="text-center">
+        <a href="https://github.com/NeuronAddict">https://github.com/NeuronAddict</a>
+      </v-col>
+      <v-col cols="4" md="4" sm="12" class="text-center">
+        2024 - Utilisation libre et non commerciale
+      </v-col>
+      <v-col cols="4" md="4" sm="12"  class="text-center">
+        <a href="https://neuronaddict.org">https://neuronaddict.org</a>
+      </v-col>
+      <v-col cols="12" class="text-center text-red">
+        <p>Application volontairement vulnérable  -  Ne pas exposer/deployer en production</p>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
+
+<style scoped>
+
+.fill-width-custom {
+  width: 100%;
+}
+</style>

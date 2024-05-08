78921092ff83705c85bf81d980f1e325a078aa2f
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2024-05-08 11:14:47 +0200

    Move footer

--- src/App.vue
+++ src/App.vue
@@ -35,5 +35,6 @@
 <script setup lang="ts">
 //
 import MessageList from "@/components/MessageList.vue";
-import Footer from "@/components/footer.vue";
+import Footer from "@/components/layout/footer.vue";
+import AppBar from "@/components/layout/AppBar.vue";
 </script>

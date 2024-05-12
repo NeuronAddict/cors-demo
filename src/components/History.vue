228b99c12cb5758074587072126ccd57f19f0829
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2024-05-12 19:11:27 +0200

    Review log

--- src/components/History.vue
+++ src/components/History.vue
@@ -3,25 +3,52 @@
 import type {Message} from "@/core/message";
 
 interface LogEntry {
+  type: "delete" | "add" | "done" | "undone",
   message: Message,
-  initiator: string,
+  initiator: string
 }
+
+const logs: LogEntry[] = [
+  {
+    message: {
+      author: 'Truc',
+      message: 'SDCsqdqsd',
+      dueDate: '2024-12-12'
+    },
+    initiator: 'Truc',
+    type: "delete"
+  },
+  {
+    message: {
+      author: 'Machin',
+      message: 'SDCsqdqsd',
+      dueDate: '2024-01-12'
+    },
+    initiator: 'Truc',
+    type: "add"
+  }
+];
 
 </script>
 
 <template>
-  <v-list>
 
-    <v-list-subheader>Actions log</v-list-subheader>
-    <v-list-item>
-      <v-list-item-title>Delete action</v-list-item-title>
-      <v-list-item>Truc has deleted item ...</v-list-item>
-    </v-list-item>
-    <v-list-item>
-      <v-list-item-title>Done action</v-list-item-title>
-      <v-list-item>Machin has finish item ...</v-list-item>
-    </v-list-item>
-  </v-list>
+  <v-sheet class="ma-2 pa-1">
+    <v-row>
+      <v-col cols="12">
+        <p class="text-h5">Actions log</p>
+        <hr/>
+      </v-col>
+    </v-row>
+    <v-row v-for="item in logs">
+      <v-col cols="4">
+        <p class="text-capitalize">{{ item.type }} :</p>
+      </v-col>
+      <v-col cols="8">
+        <p>{{ item.initiator }} has {{ item.type }} message "{{ item.message.message.substring(0, 15) }}..."</p>
+      </v-col>
+    </v-row>
+  </v-sheet>
 </template>
 
 <style scoped>

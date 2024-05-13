b54ed699365e68d67cb2be6af3fa804c3043bf0e
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2024-05-14 01:02:08 +0200

    typo

--- src/services/service.ts
+++ src/services/service.ts
@@ -4,7 +4,7 @@ import type {Message} from "@/core/message";
 
 function service<T extends { id: number }>(path: string) {
     return {
-        get: () =>  config.get<T[]>(`/${path}`),
+        get: () => config.get<T[]>(`/${path}`),
         post: (itemToAdd: CreateDTO<T>) => config.post(`/${path}`, itemToAdd),
         put: (newItem: T) => config.put(`/${path}/${newItem.id}`, newItem),
         delete: (itemToDelete: T) => config.delete(`/${path}/${itemToDelete.id}`)

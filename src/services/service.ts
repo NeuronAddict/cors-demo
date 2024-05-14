f515f7441f53062304fe05b5e908b88bd207e8a0
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2024-05-14 23:26:55 +0200

    Fix service strong type

--- src/services/service.ts
+++ src/services/service.ts
@@ -6,8 +6,8 @@ import type LogEntry from "@/core/log-entry";
 function service<T extends { id: number }>(path: string) {
     return {
         get: () => config.get<T[]>(`/${path}`),
-        post: (itemToAdd: CreateDTO<T>) => config.post(`/${path}`, itemToAdd),
-        put: (newItem: T) => config.put(`/${path}/${newItem.id}`, newItem),
+        post: (itemToAdd: CreateDTO<T>) => config.post<T>(`/${path}`, itemToAdd),
+        put: (newItem: T) => config.put<T>(`/${path}/${newItem.id}`, newItem),
         delete: (itemToDelete: T) => config.delete(`/${path}/${itemToDelete.id}`)
     }
 }

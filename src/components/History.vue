4269dec006a2b481c56c724953f71bdd9708f248
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2024-05-17 23:51:57 +0200

    Review style for history

--- src/components/History.vue
+++ src/components/History.vue
@@ -13,13 +13,11 @@ onMounted(() => {
 </script>
 
 <template>
-  <v-sheet>
-    <div class="ma-2 pa-3 text-h5">Actions log</div>
-    <v-list-item class="ma-2" v-for="logEntry in logStore.logs">
+  <div class="bg-surface ma-2 pa-3 text-h5">Actions log</div>
+  <v-list-item class="bg-surface ma-2" v-for="logEntry in logStore.logs">
         <v-list-item-title>{{ logEntry.type }} - {{ logEntry.initiator }}</v-list-item-title>
         <v-list-item-subtitle>{{ logEntry.initiator }} has {{ logEntry.type }} message "{{ logEntry.message.message.substring(0, 15) }}..."</v-list-item-subtitle>
       </v-list-item>
-  </v-sheet>
 </template>
 
 <style scoped>

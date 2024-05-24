0629047d911e9618a7d961c564e6f412265c87b6
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2024-05-24 23:15:57 +0200

    Rename handlers mock data

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -3,7 +3,7 @@ import type {Message} from "@/core/message";
 import type {CreateDTO} from "@/core/dto-types";
 import type LogEntry from "@/core/log-entry";
 
-const messages: Message[] = [
+export const mockMessages: Message[] = [
     { id: 1, author: "Truc", dueDate:'01/12/2024', message: "Sed elementum malesuada commodo. Vestibulum vehicula viverra vestibulum. Fusce dolor sapien, varius eu venenatis id, dictum sed dui. Praesent mauris magna, iaculis non mauris ac, bibendum facilisis metus. Sed eu elementum ligula. Maecenas eget libero metus. Pellentesque feugiat nunc eleifend, pharetra nisi a, accumsan turpis. Quisque aliquet varius aliquet. Pellentesque. "},
     { id: 2, author: "Bidule", dueDate: '16/08/2024', message: "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; In hac habitasse platea dictumst. Suspendisse auctor lacus at felis accumsan vehicula. Nam metus risus, ultrices quis tortor vel, ornare aliquet nisi. Nunc faucibus arcu velit, et rutrum elit ultricies a. Sed ut scelerisque erat. Quisque quis. "},
     { id: 3, author: "Machin", dueDate: '12/12/2025', message: "In nec tempus ex. Phasellus scelerisque eleifend mi non commodo. Ut a metus vitae nisl efficitur placerat eget at nunc. Suspendisse congue malesuada urna, sed scelerisque metus sodales at. Vestibulum vel lacus tellus. Donec tempor quam odio, et mollis justo."},
@@ -22,7 +22,7 @@ const messages: Message[] = [
     { id: 15, author: "Truc", dueDate: '07/07/2025', message: "Quisque et convallis enim, eu laoreet augue. Nullam dui sapien, faucibus et ultrices ut, porttitor id ligula. Nullam et vestibulum sapien. Nam commodo, ex vel pulvinar finibus, purus metus tristique lacus, non volutpat magna nulla quis quam. In vestibulum hendrerit odio, sit amet."},
 ]
 
-const logs: LogEntry[] = [
+export const MockLogs: LogEntry[] = [
     {
         id: 1,
         message: {
@@ -71,14 +71,14 @@ const logs: LogEntry[] = [
 
 
 const messageData = {
-    currentMessages: messages,
-    id: messages.length + 1,
+    currentMessages: mockMessages,
+    id: mockMessages.length + 1,
     nextId: () => messageData.id++
 };
 
 const logsData = {
-    currentLogs: logs,
-    id: logs.length + 1,
+    currentLogs: MockLogs,
+    id: MockLogs.length + 1,
     nextId: () => logsData.id++
 }
 

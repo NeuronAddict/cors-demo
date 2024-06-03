3cfa872a1c3b757ba46b33d43bf7e685f4fe4cb7
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2024-06-04 01:12:58 +0200

    Profile view

---
+++ src/components/ProfileView.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import {onMounted, type Ref, ref} from "vue";
+import Auth from "@/core/auth";
+import type {User} from "oidc-client-ts";
+
+const user: Ref<User | null> = ref(null);
+
+onMounted(async () => {
+  user.value = await Auth.getUser();
+});
+
+</script>
+
+<template>
+
+  <v-sheet class="pa-3 ma-3">
+
+    <p class="text-h5">Profile</p>
+
+    <v-table class="ma-2">
+      <thead>
+      </thead>
+      <tbody>
+
+      <tr>
+        <td>name:</td>
+        <td>{{ user?.profile.family_name }}</td>
+      </tr>
+      <tr>
+        <td>first name:</td>
+        <td>{{ user?.profile.given_name }}</td>
+      </tr>
+      <tr>
+        <td>email:</td>
+        <td>{{ user?.profile.email }}</td>
+      </tr>
+      </tbody>
+    </v-table>
+  </v-sheet>
+</template>
+
+<style scoped>
+
+</style>
--- src/plugins/router.ts
+++ src/plugins/router.ts
@@ -5,6 +5,7 @@ import SilentRefreshView from "@/components/SilentRefreshView.vue";
 import HelloWorld from "@/components/HelloWorld.vue";
 import UnauthView from "@/components/UnauthView.vue";
 import Auth from "@/core/auth";
+import ProfileView from "@/components/ProfileView.vue";
 
 
 const routes: Readonly<RouteRecordRaw[]> = [
@@ -33,8 +34,12 @@ const routes: Readonly<RouteRecordRaw[]> = [
         name: 'unauthenticated',
         path: '/unauthenticated',
         component: UnauthView
-    }
-    // { path: '/profile', component: ProfileView },
+    },
+    {
+        name: 'profile',
+        path: '/profile',
+        component: ProfileView
+    },
 ]
 
 export const router = createRouter({

72c4dda6c320fa6dcf05b07588677ff6b41259ad
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2024-06-06 23:45:06 +0200

    Review axios config

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,5 +1,19 @@
 import axios from "axios";
+import Auth from "@/core/auth";
 
 export default axios.create({
-    baseURL: import.meta.env.BASE_URL + 'api/v1',
+    baseURL: import.meta.env.VITE_BASE_URL,
+    transformRequest: [
+        async (data, headers) => {
+            try {
+                const user = await Auth.getUser();
+                if (user != null) headers.set('Authorization', `Bearer ${user.access_token}`);
+                console.log('headers', headers);
+                return data;
+            } catch (error) {
+                console.error('Error in transformRequest:', error);
+                throw error;
+            }
+        }
+    ]
 });

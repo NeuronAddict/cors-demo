0a895c57c47aa0cf5de73639a15dc2659c35ccc0
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2024-06-09 00:10:22 +0200

    Improve service typing

--- src/services/service.ts
+++ src/services/service.ts
@@ -2,15 +2,21 @@ import config from '@/services/config'
 import type {CreateDTO} from "@/core/dto-types";
 import type {Message} from "@/core/message";
 import type LogEntry from "@/core/log-entry";
+import type {AxiosInstance, AxiosResponse} from "axios";
 
-function service<T extends { id: number }>(path: string) {
+function service<T extends { id: number }>(axiosInstance: AxiosInstance, path: string): {
+    get: () => Promise<AxiosResponse<T[]>>;
+    post: (itemToAdd: CreateDTO<T>) => Promise<AxiosResponse<T>>;
+    put: (newItem: T) => Promise<AxiosResponse<T>>;
+    delete: (itemToDelete: T) => Promise<AxiosResponse<void>>;
+} {
     return {
-        get: () => config.get<T[]>(`/${path}`),
-        post: (itemToAdd: CreateDTO<T>) => config.post<T>(`/${path}`, itemToAdd),
-        put: (newItem: T) => config.put<T>(`/${path}/${newItem.id}`, newItem),
-        delete: (itemToDelete: T) => config.delete(`/${path}/${itemToDelete.id}`)
+        get: () => axiosInstance.get<T[]>(`/${path}`),
+        post: (itemToAdd: CreateDTO<T>) => axiosInstance.post<T>(`/${path}`, itemToAdd),
+        put: (newItem: T) => axiosInstance.put<T>(`/${path}/${newItem.id}`, newItem),
+        delete: (itemToDelete: T) => axiosInstance.delete(`/${path}/${itemToDelete.id}`)
     }
 }
 
-export const messageService = service<Message>('messages');
-export const logService = service<LogEntry>('logs');
+export const messageService = service<Message>(config, 'messages');
+export const logService = service<LogEntry>(config, 'logs');

7ccb371cd7f441149386d926b9905ce6af7b2fad
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2024-06-09 00:09:23 +0200

    Improve add message test

--- tests/spec/AddMessageForm.test.ts
+++ tests/spec/AddMessageForm.test.ts
@@ -3,6 +3,7 @@ import {messageStore} from "../../src/core/messages-store";
 import {flushPromises, mount} from "@vue/test-utils";
 import vuetify from "../../src/plugins/vuetify";
 import AddMessageForm from "../../src/components/messages/AddMessageForm.vue";
+import {logStore} from "../../src/core/logs-store";
 
 
 global.ResizeObserver = require('resize-observer-polyfill')
@@ -41,4 +42,9 @@ test('Add message', async () => {
     expect(messageStore.messages).toHaveLength(1);
     expect(messageStore.messages[0].message).toEqual('First message');
     expect(messageStore.messages[0].dueDate).toEqual('2025-01-01');
-})
+
+    expect(logStore.logs).toHaveLength(1);
+    expect(logStore.logs[0].message.message).toEqual('First message');
+    expect(logStore.logs[0].type).toEqual('add');
+    expect(logStore.logs[0].initiator).toEqual('anonymous');
+});

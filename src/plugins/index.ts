acc410844c91ae87a7d8e6fa9b4cdf940b305c11
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2024-06-11 01:18:09 +0200

    Add store plugin

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -11,9 +11,11 @@ import vuetify from './vuetify'
 import type {App} from 'vue'
 import {router} from "@/plugins/router";
 import {service} from "@/plugins/service";
+import {store} from "@/plugins/store";
 
 export function registerPlugins (app: App) {
   app.use(vuetify);
   app.use(router);
   app.use(service);
+  app.use(store);
 }

841f34316acf9cf07583a66ac5ec7c8ae46b48ea
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2024-06-11 00:06:21 +0200

    Service plugin

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -10,8 +10,10 @@ import vuetify from './vuetify'
 // Types
 import type {App} from 'vue'
 import {router} from "@/plugins/router";
+import {service} from "@/plugins/service";
 
 export function registerPlugins (app: App) {
   app.use(vuetify);
   app.use(router);
+  app.use(service);
 }
---
+++ src/plugins/service.ts
@@ -0,0 +1,13 @@
+import type {App} from "vue";
+import config from "@/services/axios-config";
+import {axiosInstanceProviderKey, logsServiceProviderKey, messageServiceProviderKey} from "@/core/provider";
+import {logService, messageService} from "@/services/service";
+
+export const service = {
+    install(app: App) {
+        const axiosInstance = config.newAxios();
+        app.provide(axiosInstanceProviderKey, axiosInstance);
+        app.provide(messageServiceProviderKey, messageService(axiosInstance));
+        app.provide(logsServiceProviderKey, logService(axiosInstance));
+    }
+};

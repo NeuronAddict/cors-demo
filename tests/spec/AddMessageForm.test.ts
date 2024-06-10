e94a0eef828ab1c3734484b9e31502c70c3ead17
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2024-06-11 01:16:24 +0200

    Fix add form test with injected store

--- tests/spec/AddMessageForm.test.ts
+++ tests/spec/AddMessageForm.test.ts
@@ -1,12 +1,16 @@
 import {expect, test} from "vitest";
-import {messageStore} from "../../src/core/store-provider";
 import {flushPromises, mount} from "@vue/test-utils";
 import vuetify from "../../src/plugins/vuetify";
 import AddMessageForm from "../../src/components/messages/AddMessageForm.vue";
-import {logStore} from "../../src/core/logs-store";
 import {logsServiceProviderKey, messageServiceProviderKey} from "../../src/core/service-provider";
 import {logService, messageService} from "../../src/services/service";
 import axiosConfig from "../../src/services/axios-config";
+import {
+    logStoreProvider,
+    logStoreProviderKey,
+    messageStoreProvider,
+    messageStoreProviderKey
+} from "../../src/core/store-provider";
 
 
 global.ResizeObserver = require('resize-observer-polyfill')
@@ -19,12 +23,17 @@ test('Add message', async () => {
         return config;
     });
 
+    const messageStore = messageStoreProvider();
+    const logStore = logStoreProvider();
+
     const wrapper = mount(AddMessageForm, {
         global: {
             plugins: [vuetify],
             provide: {
                 [messageServiceProviderKey]: messageService(axiosInstance),
-                [logsServiceProviderKey]: logService(axiosInstance)
+                [logsServiceProviderKey]: logService(axiosInstance),
+                [messageStoreProviderKey]: messageStore,
+                [logStoreProviderKey]: logStore
             }
         },
         props: {
@@ -51,12 +60,12 @@ test('Add message', async () => {
     await Promise.resolve().then(() => console.log(messageStore.messages));
     await flushPromises()
 
-    expect(messageStore.messages).toHaveLength(1);
-    expect(messageStore.messages[0].message).toEqual('First message');
-    expect(messageStore.messages[0].dueDate).toEqual('2025-01-01');
+    expect(messageStore.items).toHaveLength(1);
+    expect(messageStore.items[0].message).toEqual('First message');
+    expect(messageStore.items[0].dueDate).toEqual('2025-01-01');
 
-    expect(logStore.logs).toHaveLength(1);
-    expect(logStore.logs[0].message.message).toEqual('First message');
-    expect(logStore.logs[0].type).toEqual('add');
-    expect(logStore.logs[0].initiator).toEqual('anonymous');
+    expect(logStore.items).toHaveLength(1);
+    expect(logStore.items[0].message.message).toEqual('First message');
+    expect(logStore.items[0].type).toEqual('add');
+    expect(logStore.items[0].initiator).toEqual('anonymous');
 });

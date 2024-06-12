847dc089b08c8a16298b3e9c76b08c10e81a7645
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2024-06-13 01:05:00 +0200

    Improve unauth view

--- src/components/UnauthView.vue
+++ src/components/UnauthView.vue
@@ -1,9 +1,23 @@
 <script setup lang="ts">
 
+import Auth from "@/core/auth";
 </script>
 
 <template>
-  <p>Please login</p>
+  <v-row>
+    <v-col sm="12" md="8">
+      <v-sheet class="ma-3">
+        <p class="text-h5 text-center">Unauthenticated</p>
+        <hr/>
+        <p class="ma-5 text-center">Please click
+          <v-btn @click="() => Auth.signinRedirect()">here</v-btn>
+          for login
+        </p>
+
+      </v-sheet>
+    </v-col>
+    <v-col sm="12" md="4"></v-col>
+  </v-row>
 </template>
 
 <style scoped>
